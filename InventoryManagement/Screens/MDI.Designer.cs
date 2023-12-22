b9534f663e33858c1e0cdf2bcd7a280d77d9ec4f
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2023-12-22 15:37:23 +0500

    added Manu Strip

--- InventoryManagement/Screens/MDI.Designer.cs
+++ InventoryManagement/Screens/MDI.Designer.cs
@@ -31,12 +31,19 @@
             myMenu = new MenuStrip();
             fileToolStripMenuItem = new ToolStripMenuItem();
             exitToolStripMenuItem = new ToolStripMenuItem();
+            systemToolStripMenuItem = new ToolStripMenuItem();
+            productsToolStripMenuItem = new ToolStripMenuItem();
+            customerToolStripMenuItem = new ToolStripMenuItem();
+            supplierToolStripMenuItem = new ToolStripMenuItem();
+            reportsToolStripMenuItem = new ToolStripMenuItem();
+            utilitiesToolStripMenuItem = new ToolStripMenuItem();
+            usersToolStripMenuItem = new ToolStripMenuItem();
             myMenu.SuspendLayout();
             SuspendLayout();
             // 
             // myMenu
             // 
-            myMenu.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            myMenu.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, systemToolStripMenuItem, productsToolStripMenuItem, customerToolStripMenuItem, supplierToolStripMenuItem, reportsToolStripMenuItem, utilitiesToolStripMenuItem });
             myMenu.Location = new Point(0, 0);
             myMenu.Name = "myMenu";
             myMenu.Size = new Size(1235, 24);
@@ -53,9 +60,52 @@
             // exitToolStripMenuItem
             // 
             exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new Size(93, 22);
+            exitToolStripMenuItem.Size = new Size(180, 22);
             exitToolStripMenuItem.Text = "Exit";
             exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // systemToolStripMenuItem
+            // 
+            systemToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { usersToolStripMenuItem });
+            systemToolStripMenuItem.Name = "systemToolStripMenuItem";
+            systemToolStripMenuItem.Size = new Size(57, 20);
+            systemToolStripMenuItem.Text = "System";
+            // 
+            // productsToolStripMenuItem
+            // 
+            productsToolStripMenuItem.Name = "productsToolStripMenuItem";
+            productsToolStripMenuItem.Size = new Size(66, 20);
+            productsToolStripMenuItem.Text = "Products";
+            // 
+            // customerToolStripMenuItem
+            // 
+            customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            customerToolStripMenuItem.Size = new Size(76, 20);
+            customerToolStripMenuItem.Text = "Customers";
+            // 
+            // supplierToolStripMenuItem
+            // 
+            supplierToolStripMenuItem.Name = "supplierToolStripMenuItem";
+            supplierToolStripMenuItem.Size = new Size(67, 20);
+            supplierToolStripMenuItem.Text = "Suppliers";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            reportsToolStripMenuItem.Size = new Size(59, 20);
+            reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // utilitiesToolStripMenuItem
+            // 
+            utilitiesToolStripMenuItem.Name = "utilitiesToolStripMenuItem";
+            utilitiesToolStripMenuItem.Size = new Size(58, 20);
+            utilitiesToolStripMenuItem.Text = "Utilities";
+            // 
+            // usersToolStripMenuItem
+            // 
+            usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            usersToolStripMenuItem.Size = new Size(180, 22);
+            usersToolStripMenuItem.Text = "Users";
             // 
             // MDI
             // 
@@ -70,7 +120,7 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MDI";
             WindowState = FormWindowState.Maximized;
-            Load += this.MDI_Load;
+            Load += MDI_Load;
             myMenu.ResumeLayout(false);
             myMenu.PerformLayout();
             ResumeLayout(false);
@@ -82,5 +132,12 @@
         private MenuStrip myMenu;
         private ToolStripMenuItem fileToolStripMenuItem;
         private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem systemToolStripMenuItem;
+        private ToolStripMenuItem usersToolStripMenuItem;
+        private ToolStripMenuItem productsToolStripMenuItem;
+        private ToolStripMenuItem customerToolStripMenuItem;
+        private ToolStripMenuItem supplierToolStripMenuItem;
+        private ToolStripMenuItem reportsToolStripMenuItem;
+        private ToolStripMenuItem utilitiesToolStripMenuItem;
     }
 }
